2d5762ab2714883ce9ae473dafef5900d77d39ee
Mo Milan <momilan@MacBook-Pro-3.local>
2023-02-28 13:06:32 -0600

    Added 2nd SQL file

---
+++ insert_records.sql
@@ -0,0 +1,7 @@
+INSERT INTO datacamp_courses(course_name, course_instructor, topic)
+VALUES
+	('Deep Learning in Python', 'Dan Becker', 'Python');
+
+INSERT INTO datacamp_courses(course_name, course_instructor, topic)
+VALUES
+	('Joining Data in PostgreSQL', 'Chester Ismay', 'SQL');
